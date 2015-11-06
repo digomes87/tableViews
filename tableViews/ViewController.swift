7cecc5e0ab452cd796fcc706ba337d1b48421f43
Nylon <diego.gomes87@gmail.com>
2015-11-06 14:59:01 -0200

    Uso de TableViewsCell

--- tableViews/ViewController.swift
+++ tableViews/ViewController.swift
@@ -8,8 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    
+    let devCourses   = [
+        
+        ("IOS App dev With Swift","Diego go"),
+        ("Maths Curs for sciences and who like this subject","The Maths"),
+        ("Nerdologia,vlog in Youtube for Nerd or person that  likes science","Atila"),
+        ("Felipe Neto,show for everyone that social network is amazing but you can make someone bad for you if that you don't know that is soaciela network","Felipe Neto"),
+        ("I don't like cats but sometimes cats are amezing","Me"),
+        ("Goku are the the Man more Power Full Of Universy","Fan")
+    ]
+    
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return devCourses.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        let (courseTitle,courseAuthor) = devCourses[indexPath.row]
+            cell.textLabel?.text = courseTitle
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
